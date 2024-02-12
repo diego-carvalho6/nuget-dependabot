a2018b68d7bf61e2a41e93381b32cf3ddf30dae0
diego <diego.oliveira@bornlogic.com>
2024-02-12 19:32:28 -0300

    d

--- Bornlogic.NugetDependabot/Entities/Constants.cs
+++ Bornlogic.NugetDependabot/Entities/Constants.cs
@@ -7,10 +7,13 @@ public static class Constants
     public const string DefaultSpecsSeparator = " ";
     public const string DefaultSpecsDataSeparator = "=";
     public const string DefaultNugetSource = "https://api.nuget.org/v3/";
+    public const string DefaultNugetRegistry = "registration5-gz-semver2";
     public const string DefaultUrlSlash = "/";
     public const string DefaultHiddenDirectoryPrefix = ".";
     public const string DefaultDirectory = "/home/diego/RiderProjects/ActionDependencies";
     public const string DefaultIndexJsonValue = "index.json";
     public const string BasicHeader = "Basic ";
     public const string AuthorizationHeader = "Authorization";
+    public const string NugetValidType = "catalog:CatalogPage";
+    public const string DefaultVersionSeparator = ".";
 }
